e5497399892cabd0e85effa61ede22f37d4abb69
Daniel Nagel <danielnagel@mailbox.org>
2024-06-03 08:08:12 +0200

    added loop

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,26 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Bitte gib deine Schätzung ein.");
+    loop {
+        println!("Bitte gib deine Schätzung ein.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Fehler beim Lesen der Zeile");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fehler beim Lesen der Zeile");
 
-    let guess: u32 = guess.trim().parse().expect("Bitte gib eine Zahl ein!");
+        let guess: u32 = guess.trim().parse().expect("Bitte gib eine Zahl ein!");
 
-    println!("Du hast geschätzt: {guess}");
+        println!("Du hast geschätzt: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Zu klein!"),
-        Ordering::Greater => println!("Zu groß!"),
-        Ordering::Equal => println!("Du hast gewonnen!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Zu klein!"),
+            Ordering::Greater => println!("Zu groß!"),
+            Ordering::Equal => {
+                println!("Du hast gewonnen!");
+                break;
+            }
+        }
     }
 }
