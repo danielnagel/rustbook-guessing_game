7a83f336d73ddfbb8c7cb37d084775b48dce04ce
Daniel Nagel <danielnagel@mailbox.org>
2024-06-03 08:05:41 +0200

    added comparison

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
     println!("Rate die Zahl!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Die geheime Zahl ist: {secret_number}");
 
     println!("Bitte gib deine Schätzung ein.");
 
@@ -16,5 +14,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Fehler beim Lesen der Zeile");
 
+    let guess: u32 = guess.trim().parse().expect("Bitte gib eine Zahl ein!");
+
     println!("Du hast geschätzt: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Zu klein!"),
+        Ordering::Greater => println!("Zu groß!"),
+        Ordering::Equal => println!("Du hast gewonnen!"),
+    }
 }
